99cbb0896855661ea96864ee960604cf32a62c21
Dev-Bobbie <bobbie_liu88@163.com>
2019-05-02 12:46:28 +0800

    update infoq_seed and infoq_details spider

--- tool/format_headers.py
+++ tool/format_headers.py
@@ -1,15 +1,13 @@
 headers = """
-accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3
-accept-encoding: gzip, deflate, br
-accept-language: zh-CN,zh;q=0.9
-cache-control: max-age=0
-content-length: 2241
-content-type: application/x-www-form-urlencoded
-cookie: ASP.NET_SessionId=rnpemlbnv0mgy2dnppik35ek; FirstVisit=2019/4/3 10:27:34; _uab_collina=155425845464800163441555; comefrom=https://www.cmd5.com/?md5_str=5d41402abc4b2a76b9719d911017c592&button=md5%d4%da%cf%df+%bc%d3%c3%dc/%bd%e2%c3%dc; Hm_lvt_0b7ba6c81309fff7ce4498ec7b107c0b=1554264312,1554264635,1554264956,1554266675; Hm_lpvt_0b7ba6c81309fff7ce4498ec7b107c0b=1554267787
-origin: https://www.cmd5.com
-referer: https://www.cmd5.com/
-upgrade-insecure-requests: 1
-user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9
+Cache-Control: max-age=0
+Connection: keep-alive
+Cookie: BIDUPSID=9401EF4C8B5165B65D133B8A9F93DF6D; BAIDUID=1DB5FBCD3A30AEE43CD9C933044789F4:FG=1; PSTM=1553177180; BD_UPN=123253; BDUSS=dNLVcwazY2ak01Z3ZXNkxxZ1o3S0JUa3JKcXVvejlyUkNsbnV4Wmtnak4tTDljQVFBQUFBJCQAAAAAAAAAAAEAAAAH3PcqYTE1MzI0NzYwNWxvdmUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM1rmFzNa5hcRz; BDORZ=FFFB88E999055A3F8A630C64834BD6D0; delPer=0; BD_CK_SAM=1; PSINO=5; locale=zh; BDRCVFR[pNjdDcNFITf]=mk3SLVN4HKm; H_PS_645EC=0171KvQilPnZeDdfzTx1%2BfcuPJaY4s%2BjqNmj6crBNTUU6uCocmuCImOXzcRJh812QVsKHw; BD_HOME=1; H_PS_PSSID=28882_1435_28804_21111_28771_28721_28964_28837_28584_28890
+Host: www.baidu.com
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36
 """
 
 hs = headers.split('\n')
